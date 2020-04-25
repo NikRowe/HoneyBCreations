7a50b0dab977bd5a923980f7b1afe334372dae1f
Nik Rowe <NikRowe7@Gmail.com>
2020-04-24 22:42:12 -0700

    Let there be code.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Mad Hatter Programming </h1>
+      <h2>Let the insanity begin! </h2>
+    </div>
+  );
+}
